ecc92660d410123a49611e0f3a67b231959fb611
mickaelicoptere <mickael.lanier@etu.unice.fr>
2017-05-11 21:13:04 +0200

    tests logins

--- src/project/Controllers/Controller.java
+++ src/project/Controllers/Controller.java
@@ -2,12 +2,59 @@ package project.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import project.Main;
 
+import java.lang.String;
 
-public class Controller {
+import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class Controller implements Initializable {
+
+    @FXML
+    private JFXButton connexion;
+
+    @FXML
+    public JFXTextField user;
+
+    @FXML
+    public JFXPasswordField pass;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    void MakeLogin(ActionEvent event) throws Exception {
+        String username = user.getText();
+        String password = pass.getText();
+        if (username.equals("test") && password.equals("test")) {
+            toMenus();
+        } else {
+            System.out.println("Mauvais identifiants");
+        }
+    }
 
     public void toMenus() throws Exception {
         FXMLLoader loader = new FXMLLoader(
@@ -64,5 +111,4 @@ public class Controller {
         Scene scene = new Scene(root);
         Main.primaryStage.setScene(scene);
     }
-
 }
